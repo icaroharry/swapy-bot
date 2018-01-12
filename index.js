2bc0c6999657807a1847556ea21581d0b9b1baee
Ícaro Harry <me@icaro.io>
2018-01-12 00:39:11 -0200

    Implement /whitepaper

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 const TOKEN = process.env.TELEGRAM_TOKEN;
 const TelegramBot = require('node-telegram-bot-api');
 const options = {
-  webHook: {
-    // Port to which you should bind is assigned to $PORT variable
-    // See: https://devcenter.heroku.com/articles/dynos#local-environment-variables
     port: process.env.PORT
-    // you do NOT need to set up certificates since Heroku provides
-    // the SSL certs already (https://<app-name>.herokuapp.com)
-    // Also no need to pass IP because on Heroku you need to bind to 0.0.0.0
   }
 };
-// Heroku routes from port :443 to $PORT
-// Add URL of your app to env variable or enable Dyno Metadata
-// to get this automatically
-// See: https://devcenter.heroku.com/articles/dyno-metadata
-const url = process.env.APP_URL || 'https://<app-name>.herokuapp.com:443';
+
+const url = process.env.APP_URL;
 const bot = new TelegramBot(TOKEN, options);
 
 
@@ -22,8 +13,11 @@ const bot = new TelegramBot(TOKEN, options);
 // Note: we do not need to pass in the cert, as it already provided
 bot.setWebHook(`${url}/bot${TOKEN}`);
 
-
-// Just to ping!
 bot.onText(/\/presale/, (msg, match) => {
   bot.sendMessage(msg.chat.id, 'The pre-sale will start in mid February, follow our official announcements at https://t.me/SwapyNetwork');
+});
+
+bot.onText(/\/whitepaper/, (msg, match) => {
+  bot.sendMessage(msg.chat.id, 'Swapy Network Whitepaper v0.98.2');
+  bot.sendDocument(msg.chat.id, 'files/whitepaper.pdf');
 });
